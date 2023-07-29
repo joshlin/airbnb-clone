43eb685d4e113aefad920dd268dd7a028a67ee33
Josh Lin <joshlin@uw.edu>
2023-07-29 00:50:47 -0700

    first commite

---
+++ app/components/Container.tsx
@@ -0,0 +1,31 @@
+'use client';
+// import { Container } from "postcss";
+// import React from "react";
+
+
+import React from "react";
+
+interface Container {
+    children: React.ReactNode;
+}
+
+const Container: React.FC<Container> = ({
+    children 
+}) => {
+    return (  
+        <div
+            className="
+                max-w-[2520px]
+                mx-auto
+                xl:px-20
+                md:px-10
+                sm:px-2
+                px-4
+            "
+        >
+            {children}
+        </div>
+    );
+}
+
+export default Container;
